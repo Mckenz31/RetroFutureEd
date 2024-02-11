19e5e42ea6cf444b8a6b8d3a39977ed9612b5062
Mckenzie <mckenz318@gmail.com>
2024-02-11 04:19:09 -0600

    minor: sending link from r2-d2 to backend

--- fend/lib/pages/r2-d2.dart
+++ fend/lib/pages/r2-d2.dart
@@ -1,5 +1,7 @@
 import 'package:fend/pages/chat.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class R2D2 extends StatefulWidget {
   const R2D2({super.key});
@@ -60,7 +62,16 @@ class _R2D2 extends State<R2D2> {
                 hintStyle: const TextStyle(color: Color.fromARGB(255, 95, 93, 216)),
                 suffixIcon: IconButton(
                   icon: const Icon(Icons.send),
-                  onPressed: () {
+                  onPressed: () async{
+                    await http.post(
+                      Uri.parse('http://localhost:5000/handle_userinput'),
+                      headers: <String, String>{
+                        'Content-Type': 'application/json; charset=UTF-8',
+                      },
+                      body: jsonEncode(<String, String>{
+                        'title': messageController.text,
+                      }),
+                    );
                     Navigator.push(context, MaterialPageRoute(builder: (context) => Chat(ai: 'R2-D2',)));
                     messageController.clear();     
                   },            
