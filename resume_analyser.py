c623b80f7fec5c5b8eb7610081dcbfcf7fea63ab
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-02-11 04:07:45 -0600

    Fix Resume_analyser

--- resume_analyser.py
+++ resume_analyser.py
@@ -10,8 +10,8 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_repsonse(input):
-    model=genai.GenerativeModel('gemini-pro')
-    response=model.generate_content(input)
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input)
     return response.text
 
 def input_pdf_text(uploaded_file):
@@ -24,15 +24,15 @@ def input_pdf_text(uploaded_file):
 
 #Prompt Template
 
-input_prompt="""
+input_prompt = """
 Hey Act Like a skilled or very experience ATS(Application Tracking System)
 with a deep understanding of tech field,software engineering,data science ,data analyst
 and big data engineer. Your task is to evaluate the resume based on the given job description.
 You must consider the job market is very competitive and you should provide 
 best assistance for improving the resumes. Assign the percentage Matching based 
 on Jd and the missing keywords with high accuracy
-resume:{text}
-description:{jd}
+resume:{resume_info}
+description:{job_description}
 
 I want the response in one single string having the structure
 {{"JD Match":"%","MissingKeywords:[]","Profile Summary":"", "Areas of improvement":""}}
@@ -41,13 +41,15 @@ I want the response in one single string having the structure
 ## streamlit app
 st.title("Smart ATS")
 st.text("Improve Your Resume ATS")
-jd=st.text_area("Paste the Job Description")
-uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
+jd = st.text_area("Paste the Job Description")
+uploaded_file = st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
+
 
 submit = st.button("Submit")
 
 if submit:
     if uploaded_file is not None:
         text=input_pdf_text(uploaded_file)
-        response=get_gemini_repsonse(input_prompt)
+        formatted_input_prompt = input_prompt.format(resume_info = text, job_description = jd)
+        response = get_gemini_repsonse(formatted_input_prompt)
         st.subheader(response)
