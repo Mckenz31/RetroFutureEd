ef3ba4ba55b511783519844703710df40cadb6ec
Mckenzie <mckenz318@gmail.com>
2024-02-11 01:17:14 -0600

    D2R2-links & C3PO-documents

--- fend/lib/data.dart
+++ fend/lib/data.dart
@@ -2,10 +2,26 @@ class Data {
   final Map<String, String> imageDictionary = {
     'Ada Lovelace': 'ada_lovelace.jpg',
     'Alan Turing': 'alan_turing.jpg',
+    'Annie Easley': 'annie_easley.jpg',
+    'Charles Babbage': 'barles_cabbage.jpg',
+    'Elizabeth Blackwell': 'elizabeth_blackwell.jpg',
+    'Emile Baudot': 'emile_baudot.jpg',
+    'Fredrico Faggin': 'fredrico_faggin.jpg',
     'Grace Hopper': 'grace_hopper.jpg',
+    'Herman Hollerit': 'herman_hollerit.jpg',
+    'Issac Newton': 'issac_newton.jpg',
+    'James Maxwell': 'james_maxwell.jpg',
+    'John Backus': 'john_backus.jpg',
+    'Joseph Wizenbaum': 'joseph_wizenbaum.jpg',
+    'Katherine Johnson': 'katherine_johnson.jpg',
+    'Konard Zuze': 'konard_zuze.jpg',
+    'Leonardo Torres': 'leonardo_torres.jpg',
     'Marie Curie': 'marie_curie.jpg',
+    'Maurice Vincent Wilkes': 'maurice_vincent_wilkes.jpg',
     'Nikola Tesla': 'nikola_tesla.jpg',
     'Rosalind Franklin': 'rosalind_franklin.jpg',
-    'Stephen Hawking': 'stephen_hawking.jpg'
+    'Seymour Roger Cray': 'seymour_roger_cray.jpg',
+    'Stephen Hawking': 'stephen_hawking.jpg',
+    'Von Neumann': 'von_neumann.jpg'
   };
 }
